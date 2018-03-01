8c79ffa06adc4c7721d044ed4ac603c8f95ef47d
Muchlas Mughniy <muchlas.muchlas@gmail.com>
2018-03-01 17:04:18 +0700

    Change Structure

---
+++ SekolahApp.DataModel/BaseTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SekolahApp.DataModel
+{
+    public class BaseTable
+    {
+        [Column(TypeName = "varchar"), MaxLength(30)]
+        public string CreatedBy { get; set; }
+
+        public DateTime Created { get; set; }
+
+        [Column(TypeName = "varchar"), MaxLength(30)]
+        public string ModifiedBy { get; set; }
+
+        public DateTime? Modified { get; set; }
+    }
+}
